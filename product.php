546e6bc021b45e88d0b3aec3b5b8943cffdc3078
Dipkumar Hareshkumar Patel <95977745+dippatel1@users.noreply.github.com>
2022-05-02 15:29:36 +0530

    Add files via upload
    final

---
+++ product.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <style>
+    .product_card {
+        width: fit-content;
+        height: fit-content;
+        background-color: aliceblue;
+        padding: 2 1rem;
+
+    }
+
+    .product_image {
+
+        border-radius: 2rem;
+        padding: 1rem;
+    }
+
+    img {
+        width: 10rem;
+        height: 10rem;
+        border-radius: 1rem;
+        ;
+    }
+
+    .product_details {
+        text-align: center;
+    }
+
+    h3,
+    h2 {
+        margin: 0;
+    }
+
+    .container {
+        display: flex;
+        flex-flow: column;
+    }
+    </style>
+
+</head>
+
+<body>
+
+    <div class="cintainer">
+        <div class="product_card">
+
+
+            <div class="product_image">
+                <img src="1.jpeg" alt="">
+            </div>
+            <div class="product_details">
+                <h3>Watch</h3>
+                <p>Rolex</p>
+                <h2>20$</h2>
+            </div>
+        </div>
+        <div class="product_card">
+
+
+            <div class="product_image">
+                <img src="1.jpeg" alt="">
+            </div>
+            <div class="product_details">
+                <h3>Watch</h3>
+                <p>Rolex</p>
+                <h2>20$</h2>
+            </div>
+        </div>
+        <div class="product_card">
+
+
+            <div class="product_image">
+                <img src="1.jpeg" alt="">
+            </div>
+            <div class="product_details">
+                <h3>Watch</h3>
+                <p>Rolex</p>
+                <h2>20$</h2>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
